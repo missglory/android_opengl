a393497dfedfdc2ee5b880c7252e853f114bc8f8
missglory <xhaustful@gmail.com>
2019-02-15 18:23:27 +0300

    JNI GLES

--- app/src/main/cpp/game.cpp
+++ app/src/main/cpp/game.cpp
@@ -5,14 +5,18 @@
 #include "game.h"
 #include "glwrapper.h"
 #include <GLES2/gl2.h>
+#include <iostream>
 void on_surface_created() {
     glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+//    std::cout << "hwa\n";
 }
 
 void on_surface_changed() {
     // No-op
+//    std::cout << "hwc\n";
 }
 
 void on_draw_frame() {
     glClear(GL_COLOR_BUFFER_BIT);
+//    std::cout << "hww\n";
 }
